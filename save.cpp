f9146fdc625c1a9da8af71684a65376fb688aff6
Elijah Littell <elittell214@gmail.com>
2020-12-28 00:39:45 -0500

    added a if statment if you bought or not

--- save.cpp
+++ save.cpp
@@ -4,26 +4,35 @@
 // See how much $ I already have
 // _ amount always in the bank for auto pay fees
 
-double myBank(double savingsAmount) {
-    double autoPay = 51.38;
-    int car = 3000;
+double savings(double savingsAmount = 2200) {
+    double autoPayments = 51.38;
+    std::string yesOrNo = "";
+    double priceOfItem;
+
     std::cout << "You currently have $" << savingsAmount << " before any deductions" << "\n";
-    savingsAmount = savingsAmount - autoPay;
-    std::cout << "You now have $" << savingsAmount << " after your autopay on Netflix and Adobe" << "\n";
+    savingsAmount -= autoPayments;
+
+    std::cout << "You now have $" << savingsAmount << " after your autopayments on Netflix and Adobe" << "\n";
+
+    std::cout << "Have you bought anything in the past week?" << "\n";
+    std::cin >> yesOrNo;
+
+    if (yesOrNo == "no") {
+        return true;
+    } else if (yesOrNo == "yes") {
+        std::cout << "How much was the item?" << "\n";
+        std::cin >> priceOfItem;
+        savingsAmount -= priceOfItem;
+        std::cout << "You now have $" << savingsAmount << " left in your savings" << "\n";
+    }
+
 
     return savingsAmount;
 }
 
 int main() {
 
-    std::cout << "If you want to save a good amount for stocks before the age of 18 then you should save 10% of your income" << "\n";
-
-    double myTotal = myBank(200);
-    std::cout << myTotal;
-
-    double stocksAmount = myTotal * 0.1;
-
-    std::cout << "\n" << stocksAmount << "\n";
+    std::cout << savings() << "\n";
 
     return 0;
 }
