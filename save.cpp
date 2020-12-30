27189b6f65c6a18437e1c62eeb800fbb575595ec
Elijah Littell <elittell214@gmail.com>
2020-12-30 15:56:08 -0500

    added a while statement for a pin

--- save.cpp
+++ save.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 double savings(double savingsAmount = 2200) {
     double autoPayments = 51.38;
+    double priceOfItem;
     string yesOrNo = "";
     string yesOrNoAutoPayments;
-    double priceOfItem;
 
     cout << "You currently have $" << savingsAmount << " before any deductions" << "\n";
     cout << "Did you pay the autopayments yet? ";
@@ -32,8 +32,23 @@ double savings(double savingsAmount = 2200) {
 }
 
 int main() {
+    int pin = 1010;
+    int pinAnswer;
+    int tries = 3;
+    while(pinAnswer != pin && tries > 0) {
+        cout << "What is the pin? ";
+        cin >> pinAnswer;
+        tries--;
+        if (tries != 0 && pinAnswer != pin) {
+            cout << "Incorrect, you now have " << tries << " tries left.\n";
+        } else if (tries == 0) {
+            cout << "You are now locked out! Contact our specialist for help.\n";
+        } else {
+            break;
+        }
+    }
 
-    cout << savings() << "\n";
+    // cout << savings() << "\n";
 
     return 0;
 }
