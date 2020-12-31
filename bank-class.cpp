b13d251a06de9f31f01aca7b4c6d859796a59b40
Elijah Littell <elittell214@gmail.com>
2020-12-31 17:14:38 -0500

    Put my class in this file

---
+++ bank-class.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+class Bank {
+    public:
+        void signIn() {
+            int pin = 1010;
+            int pinAnswer;
+            int tries = 3;
+            while(pinAnswer != pin && tries > 0) {
+                cout << "What is the pin? ";
+                cin >> pinAnswer;
+                tries--;
+                if (tries != 0 && pinAnswer != pin) {
+                    cout << "Incorrect, you now have " << tries << " tries left.\n";
+                } else if (tries == 0 && pinAnswer != pin) {
+                    cout << "You are now locked out! Contact our specialist for help.\n";
+                } else {
+                    cout << "Correct!\n";
+                    string checkingsOrSavings;
+                    cout << "Welcome! would you like to go into your checkings or savings?\n";
+                    cin >> checkingsOrSavings;
+
+                    if (checkingsOrSavings == "checkings") {
+                        checkings();
+                    } else if (checkingsOrSavings == "savings") {
+                        savings(2020);
+                    } else {
+                        cout << "try again!";
+                    }
+                }
+            }
+        }
+
+        void savings(double savingsAmount) {
+            savingsAmount = 2200;
+            double autoPayments = 51.38;
+            double priceOfItem;
+            string yesOrNo = "";
+            string yesOrNoAutoPayments;
+
+            cout << "You currently have $" << savingsAmount << " before any deductions" << "\n";
+            cout << "Did you pay the autopayments yet? ";
+            cin >> yesOrNoAutoPayments;
+            if (yesOrNoAutoPayments == "yes") {
+                savingsAmount -= autoPayments;
+            }
+
+            cout << "You now have $" << savingsAmount << " after your autopayments on Netflix and Adobe" << "\n";
+            cout << "Have you bought anything in the past week? ";
+            cin >> yesOrNo;
+
+            if (yesOrNo == "no") {
+                cout << "You now have " << savingsAmount << " in your account\n";
+            } else if (yesOrNo == "yes") {
+                cout << "How much was the item? ";
+                cin >> priceOfItem;
+                savingsAmount -= priceOfItem;
+                cout << "You now have $" << savingsAmount << " left in your savings" << "\n";
+            }
+        }
+
+        void checkings() {
+            cout << "120.03";
+        }
+};
